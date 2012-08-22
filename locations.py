0b90c1c7da6d5560327582abc862e36b4843144b
Bluedagger21 <shobiubikadobi@gmail.com>
2012-08-22 01:11:12 -0700

    Completely removed the realize function.
    Was anything in there planned to be required? I modified checkLoc to use
    the range function to test location. Thought it might be a bit more
    efficient!

--- locations.py
+++ locations.py
@@ -1,67 +1,5 @@
 import messages
 import movement
-# Define your locations below this function definition
-
-
-def realize(ty, coords):
-    """
-    "realize" = to find all of the coords inside an area
-    Finds them by laying-out the coords in coordX/coordsY,
-    getting the lowest and highest elements, and returning
-    the possible coords.
-    There are two modes:
-        1: Finds each coordinate within the area
-        2: Finds just the coordinates of the edges of the area
-    coords = [[2, 4],[6, 4]]
-    ###       X^  ^Y
-    """
-    coordsx = []
-    coordsy = []
-    for i, j in coords:
-        coordsx.append(i)  # append left
-        coordsy.append(j)  # append right
-
-    # checking if 2 or 4 points were given
-    if len(coordsx) == 2:
-        h = 1
-    elif len(coordsx) == 4:
-        h = 3
-    else:
-        exit("ERROR")
-    coordsx.sort()   # Easily getting lowest and highest
-    lx = coordsx[0]  # lowest-X
-    hx = coordsx[h]  # highest-X
-    coordsy.sort()
-    ly = coordsy[0]  # same for y
-    hy = coordsy[h]
-
-    if lx > hx or lx == hx:  # sanity check
-        exit("ERROR: In realize")  # Need to work on better error messages
-    elif ly > hy or ly == hy:
-        exit("ERROR: In realize")
-
-    # resetting coords list to accept new values
-    coords = []
-    if ty == 1:
-        lyr = ly   # low-Right reset
-        while lx <= hx:
-            while ly <= hy:
-                coords.append([lx, ly])
-                ly += 1
-            lx += 1
-            ly = lyr
-    elif ty == 2:
-        # Goes around the edges of the area.
-        for x in range(lx, hx + 1):
-            coords.append([x, ly])
-        for y in range(ly, hy + 1):
-            coords.append([hx, y])
-        for x in range(hx, lx - 1, -1):
-            coords.append([x, hy])
-        for y in range(hy, ly - 1, -1):
-            coords.append([lx, y])
-
-    return coords
 
 ###################################
 # Define borders for locations here
@@ -71,9 +9,10 @@ def realize(ty, coords):
 forest1 = [[0, 5], [5, 10]]
 bigTree1 = [[3, 7]]
 
-forest1Outline = realize(2, forest1)
-forest1 = realize(1, forest1)
-
+#forest1Outline = realize(2, forest1)
+#print forest1
+#forest1 = realize(1, forest1)
+#print forest1
 
 def Fforest1(xy):
     from game import ui
@@ -91,6 +30,12 @@ def checkLoc(xy):
     appropriate function as needed.
     """
     #xy = [[movement.x, movement.y]]
-    if xy in forest1:
-        Fforest1(xy)
+    print xy
+    #These nested ifs could be combined with an and for less code
+    if xy[0] in range(forest1[0][0],forest1[1][0]):
+        #x is in range
+        if xy[1] in range(forest1[0][1],forest1[1][1]):
+            #y is in range
+            print "in y range"
+            Fforest1(xy)
     #del(xy)
